315e12f12ab0ded0fc7fe2d4d7e95b7f98dd1795
jap18021 <144380826+jap18021@users.noreply.github.com>
2024-01-29 22:00:21 -0500

    Create non_prefix_sum.h

---
+++ non_prefix_sum.h
@@ -0,0 +1,9 @@
+#ifndef NON_PREFIX_SUM_H
+#define NON_PREFIX_SUM_H
+
+#include <vector>
+
+bool non_neg_prefix_sum(const std::vector<int>& lst);
+bool non_pos_prefix_sum(const std::vector<int>& lst);
+
+#endif
