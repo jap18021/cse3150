160d755639829fbce9abd1532d8252f07fa0655e
jap18021 <144380826+jap18021@users.noreply.github.com>
2024-01-29 22:46:18 -0500

    Update non_prefix_sum.cpp

--- lab1/non_prefix_sum.cpp
+++ lab1/non_prefix_sum.cpp
@@ -1,48 +1,38 @@
 #include "prefix_sum.h"
 #include <iostream>
 
-int prefix_sum(int ary[], int length)
-{
-    int sum = 0;
-    for (int i = 0; i < length; i++)
-    {
-        sum += ary[i];
-    }
-    return sum;
-}
-
-bool non_positive_prefix(int ary[], int length)
-{
-    bool non_positive = true;
-    int sum = 0;
-
-    for (int index = 0; index < length; index++)
-    {
-        sum += ary[index];
-        if (sum > 0)
-        {
-            non_positive = false;
-            break;
-        }
-    }
-
-    return non_positive;
-}
-
-bool non_negative_prefix(int ary[], int length)
-{
+bool non_negative_prefix(int ary[], int length) {
     bool non_negative = true;
     int sum = 0;
-
-    for (int index = 0; index < length; index++)
-    {
+    
+    for (int index = 0; index < length; index++) {
         sum += ary[index];
-        if (sum < 0)
-        {
+        if (sum < 0) {
             non_negative = false;
             break;
         }
     }
-
     return non_negative;
+}
+
+bool non_positive_prefix(int ary[], int length) {
+    bool non_positive = true;
+    int sum = 0;
+
+    for (int index = 0; index < length; index++) {
+        sum += ary[index];
+        if (sum > 0) {
+            non_positive = false;
+            break;
+        }
+    }
+    return non_positive;
+}
+
+int prefix_sum(int ary[], int length) {
+    int sum = 0;
+    for (int i = 0; i < length; i++) {
+        sum += ary[i];
+    }
+    return sum;
 }
