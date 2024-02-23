a91737e5419ec0d66da6fa229d4ada3383eebe21
jap18021 <144380826+jap18021@users.noreply.github.com>
2024-02-23 12:54:37 -0500

    Update prefixsum_valley.h

--- lab4/prefixsum_valley.h
+++ lab4/prefixsum_valley.h
@@ -6,6 +6,7 @@
 std::vector<int> generate_balanced_list(int n);
 std::vector<int> scramble_list(std::vector<int>& list);
 int find_lowest_depth(const std::vector<int>& list);
+std::vector<int> calculate_prefix_sums(const std::vector<int>& list);
 std::pair<std::vector<int>, std::vector<int>> split_list(const std::vector<int>& list);
 std::vector<int> create_final_list(std::vector<int>& P1, std::vector<int>& P2);
 
